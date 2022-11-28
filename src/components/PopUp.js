a17fbd6ac222dcafcc338fe1b8512ed687838889
stefania <sstefansdottir88@gmail.com>
2022-11-28 10:59:36 -0600

    pdf file added

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import popupImage from "../images/popupImage.png";
 import CityAledo from "../images/cityaledo - logo.png";
+import pdfAledo from "../images/pdfAledo.png";
+
 import '../PopUp.css'
 
 function PopUp({trigger, triggerSet}) {
@@ -9,7 +11,7 @@ function PopUp({trigger, triggerSet}) {
         <div className="popup-inner">
             <div className="row d-flex justify-content-center">
                 <div className="col-sm-4 p-0">
-                    <img src={popupImage} alt="popupImage" className='popup-image' onClick={() => window.location.href = "https://www.aledotx.gov/"}></img>
+                    <img src={popupImage} alt="popupImage" className='popup-image' onClick={() => window.location.href = "https://drive.google.com/file/d/1LNYMpeeq31Eh95Av3DQ9ZCQiEBybRTax/view?usp=share_link"}></img>
                 </div>
                 <div className="col-sm-4 popup-right p-5 p-sm-2">
                     <div className="row d-flex justify-content-center h-100">
@@ -27,11 +29,18 @@ function PopUp({trigger, triggerSet}) {
                   </a>
                 </p>
                 <h1>For more information please click on the link below</h1>
+
                 <div className="col city-logo">
-                  <a href="https://www.aledotx.gov/human-resources" target="blank">
+                  <a href="https://www.aledotx.gov/" target="blank">
+                    <img src={pdfAledo} alt="pdfAledo"></img>
+                  </a>
+                </div>
+                <div className="col city-logo">
+                  <a href="https://www.aledotx.gov/" target="blank">
                     <img src={CityAledo} alt="cityAledo"></img>
                   </a>
                 </div>
+                
                                 <button className="close-btn" onClick={() => triggerSet(false)}>close</button>
                             </div>
                         </div>
